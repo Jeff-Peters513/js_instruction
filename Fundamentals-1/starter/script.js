f285b7c0e5cfe51b4cef802f7803661a9ed7a827
Jeff-Peters513 <petersjeff288@gmail.com>
2020-12-23 15:31:36 -0500

    init commit

---
+++ Fundamentals-1/starter/script.js
@@ -0,0 +1,142 @@
+/*
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Jonas";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let PI = 3.1415;
+
+// self work//
+let country = "United States of America";
+let continent = "North America";
+let population = 360;
+let isIsland = false;
+let language;
+
+
+console.log(country);
+console.log(continent);
+console.log(population);
+console.log(isIsland);
+console.log(language);
+
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+//console.log(typeof true);
+console.log(typeof javascriptIsFun);
+//console.log(typeof 23);
+//console.log(typeof 'Jonas');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null);
+
+//declare varibles with let, var, const
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+//birthYear = 1990;
+//const job;
+
+var job = 'programmer';
+job = 'teacher';
+
+lastName = 'Schmedtman';
+console.log(lastName);
+
+
+
+
+//operators
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageSarah * 2, ageJonas / 10, 2 ** 3);
+//2**3 means 2 to the power of 3 = 2*2*2=8
+
+const firstName = 'Jonas';
+const lastName = "Schmedtman";
+console.log(firstName + ' ' + lastName);
+
+//assignment operators
+let x = 10 + 5; //15
+x += 10; //15 + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1 = 101
+x--;
+x--;
+console.log(x);
+
+//Comparison operators
+console.log(ageJonas > ageSarah);
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
+
+
+
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge);
+*/
+
+
+////////////////////////////////
+//coding challenge #1
+
+// const markMass = 78;
+// const markHeight = 1.69;
+
+// const johnMass = 92;
+// const johnHeight = 1.95;
+
+const markMass = 95;
+const markHeight = 1.88;
+
+const johnMass = 85;
+const johnHeight = 1.76;
+
+const markBMI = markMass / markHeight ** 2;
+const johnBMI = johnMass / (johnHeight * johnHeight);
+
+const markHigherBMI = markBMI > johnBMI;
+
+console.log("Mark's BMI" + ' ' + markBMI, "John's BMI" + ' ' + johnBMI);
+console.log("Is Mark's BMI higher?" + ' ' + markHigherBMI);
+
+
+
