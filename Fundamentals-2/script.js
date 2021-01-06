6e7381e894c45a5736fa66781171fa7a8f71bc2d
Jeff-Peters513 <petersjeff288@gmail.com>
2021-01-06 14:32:03 -0500

    Arrays in JS

--- Fundamentals-2/script.js
+++ Fundamentals-2/script.js
@@ -159,6 +159,79 @@ checkWinner(avgDolphins, avgKoalas);
 
 */
 
+/*
+//39. Introduction to Arrays
+
+const friend1 = "Micheal";
+const friend2 = "Steven";
+const friend3 = "Peter";
+
+const friends = ['Micheal', 'Steve', 'Peter'];
+console.log(friends);
+
+const y = new Array(1991, 1984, 2008, 2020);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+friends[2] = "Jay";
+console.log(friends);
+// friends = ['bob, 'Alice']; //can not replace the entire array only portions of it
+
+const firstName = "Jonas";
+const jonas = [firstName, 'Schmedtman', 2037 - 1991, 'teacher', friends];
+console.log(jonas);
+console.log(jonas.length);
+
+//Exercise
+const calcAge = function (birthYear) {
+    return 2037 - birthYear;
+};
+const years = [1990, 1967, 2002, 2010, 2018];
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+console.log(age1, age2, age3);
+
+const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
+console.log(ages);
+*/
+
+//40. Basic Array operations
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
