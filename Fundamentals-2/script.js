8e832b1d26ced36cbf1803b03b9d0884672d8268
Jeff-Peters513 <petersjeff288@gmail.com>
2021-01-07 13:06:25 -0500

    coding chanllenge 2 of part 2

--- Fundamentals-2/script.js
+++ Fundamentals-2/script.js
@@ -239,10 +239,27 @@ if (friends.includes('Steve')) {
 
 // Coding Challenge #2 of part 2
 
+function calcTip(bill) {
+    let perc;
+    if (bill <= 50 || bill >= 300) {
+        perc = 0.20;
+    } else {
+        perc = 0.15;
+    }
+    return perc * bill
+    //return "The tip is " + (perc * bill);
+};
 
+//let tip = calctip(10);
+//console.log(calctip(100));
 
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
 
-
+console.log(bills);
+console.log(tips);
+console.log(total);
 
 
 
