fec5d94534f4ccdb160ef7353b83fdbea4cc1706
Jeff-Peters513 <petersjeff288@gmail.com>
2021-01-10 15:21:47 -0500

    added extentions and settings and prettierrc

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,12 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+//Lecture 55. Setting up VS Code
+
+const x = 23;
+if (x === 23) console.log(23);
+
+const calcAge = (birthYear) => 2037 - birthYear;
+
+console.log('this is awesome');
+//leture 56.
