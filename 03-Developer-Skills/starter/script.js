5ba63d7e8b47c6d11af1f41169ba55a4f12bb562
Jeff-Peters513 <petersjeff288@gmail.com>
2021-01-12 13:27:18 -0500

    changes and added code

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -10,3 +10,13 @@ const calcAge = (birthYear) => 2037 - birthYear;
 
 console.log('this is awesome');
 //leture 56.
+
+const t = 45;
+console.log(t);
+console.log(t, t, t, t, x);
+console.log('I like this so much!!');
+
+// TODO finish this file
+//BUG
+//VIDEO
+//FIXME
