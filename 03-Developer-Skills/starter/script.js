16905827c7b01261b00b8005690623d5d88f34e8
Jeff-Peters513 <petersjeff288@gmail.com>
2021-01-12 15:24:08 -0500

    coding chg 1 sect 3 creating a flex string with template literals

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -20,3 +20,35 @@ console.log('I like this so much!!');
 //FIXME
 //VIDEO
 //BUG
+
+//62. Coding Challenge #1
+/*
+Given an array of forecasted maximum temperatures, the thermometer displays
+ a string with these temperatures.
+
+Example: [17, 21, 23] will print "... 17ºC in 1 days ... 21ºC in 2 days ... 23ºC in 3 days ..."
+
+Create a function 'printForecast' which takes in an array 'arr' and logs
+ a string like the above to the console.
+
+Use the problem-solving framework: Understand the problem and break it up into sub-problems!
+
+TEST DATA 1: [17, 21, 23]
+TEST DATA 2: [12, 5, -5, 0, 4]
+*/
+
+const data1 = [17, 21, 23];
+const data2 = [12, 5, -5, 0, 4];
+
+//console.log(`...${data1[0]}C...${data1[1]}C...${data1[2]}C...`);
+
+const printForecast = function (arr) {
+  console.log(arr);
+  let str = '';
+  for (let i = 0; i < arr.length; i++) {
+    str += `${arr[i]}°C in ${i + 1} days...`;
+  }
+  console.log('...' + str);
+};
+printForecast(data1);
+printForecast(data2);
