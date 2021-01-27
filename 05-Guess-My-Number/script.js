9c5d866e22bd7564a8364f4f7319848823a5c8a0
Jeff-Peters513 <petersjeff288@gmail.com>
2021-01-27 11:41:32 -0500

    finished guess my number created New modal project

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -86,7 +86,7 @@ document.querySelector('.check').addEventListener('click', function () {
   // }
 });
 
-//setting up fuction to make again button work to reset page
+//setting up fuction to make again button work to play again and keep highscore
 document.querySelector('.again').addEventListener('click', function () {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
