8bfca46d48aab18c7ae97abf25957f2040245e08
Jeff-Peters513 <petersjeff288@gmail.com>
2021-01-27 12:47:07 -0500

    working with classes on the modal project

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -1,11 +1,29 @@
 'use strict';
-
+//79 & 80 lecture video
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnsOpenModal);
 
-for (let i = 0; i < btnsOpenModal.length; i++) {
-  console.log(btnsOpenModal[i].textContent);
-}
+//function to open
+const openModal = function () {
+  console.log('Button clicked');
+  //do not need . in the below statement
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+//function to close
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+//close model window and remove overlay when clicking the close 'X'
+btnCloseModal.addEventListener('click', closeModal);
+
+//remove overlay and close model window when clicking anywhere on the overlay
+overlay.addEventListener('click', closeModal);
