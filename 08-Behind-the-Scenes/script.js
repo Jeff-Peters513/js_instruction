d51192a3005a7b75fdb06ad7af1764b2bbbf999f
Jeff-Peters513 <petersjeff288@gmail.com>
2021-02-01 10:50:33 -0500

    reg vs arrow functions with this keyword

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 function calcAge(birthYear) {
   const age = 2037 - birthYear;
 
@@ -35,3 +35,133 @@ const firstName = 'Jonas';
 calcAge(1991);
 //console.log(age);
 //printAge();
+*/
+
+//95. Hoisting and TDZ pratice
+
+//Hoisting with variables
+/*
+console.log(me);
+console.log(job);
+console.log(year);
+
+var me = 'Jonas';
+let job = 'teacher';
+const year = 1991;
+*/
+
+//Hoisting with functions
+/*
+console.log(addDecl(2, 3));
+//console.log(addExpr(2, 3));
+console.log(addArrow);
+//console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+
+//Example
+console.log(numProducts); //numProducts is undefined because it is a var and of hoisting
+if (!numProducts) deleteShoppingChart();
+
+var numProducts = 10;
+
+function deleteShoppingChart() {
+  console.log('All products deleted!');
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+*/
+
+//96. & 97. this keyword pratice
+// console.log(this); //points to global window object
+/*
+const calcAge = function (birthYear) {
+  // console.log(2037 - birthYear);
+  // console.log(this); //undefined
+};
+calcAge(1991);
+
+const calcAgeArrow = birthYear => {
+  // console.log(2037 - birthYear);
+  // console.log(this); //does not get this keyword function -  this points to the global window object
+};
+calcAgeArrow(1991);
+
+//creating an object with a this keyword
+const jonas = {
+  year: 1991,
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+  },
+};
+jonas.calcAge();
+//
+const matilda = {
+  year: 2017,
+};
+
+matilda.calcAge = jonas.calcAge; //method borrowing - this added the calcAge function from jonas to matilda object
+matilda.calcAge(); //this keyword allways points to the method calling it
+
+// const f = jonas.calcAge;
+// f(1991);
+*/
+
+//98. lecture regular functions vs arrow functions
+// var firstName = 'Matilda';
+
+const jonas = {
+  year: 1991,
+  firstName: 'Jonas',
+  calcAge: function () {
+    // console.log(this);
+    console.log(2037 - this.year);
+
+    //solution 1
+    // const self = this; //self or that - this method was used in ES5 and ealier
+    // const isMillenial = function () {
+    //   console.log(self);
+    //   console.log(self.year >= 1981 && self.year <= 1996);
+    // };
+    // isMillenial();
+
+    //solution 2
+    const isMillenial = () => {
+      console.log(this);
+      console.log(this.year >= 1981 && this.year <= 1996);
+    };
+    isMillenial();
+  },
+
+  greet: () => {
+    console.log(this);
+    console.log(`Hey ${this.firstName}`);
+  }, //arrow functions do not get the 'this' keyword function
+};
+jonas.greet();
+jonas.calcAge();
+
+//Arguments keyword
+const addExpr = function (a, b) {
+  console.log(arguments);
+  return a + b;
+};
+addExpr(2, 5);
+addExpr(2, 5, 8, 12);
+
+var addArrow = (a, b) => {
+  console.log(arguments);
+  return a + b;
+};
+addArrow(2, 5, 8);
