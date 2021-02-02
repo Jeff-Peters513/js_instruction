c84a5e910f4023274ed5039708f99d87e38fa4f5
Jeff-Peters513 <petersjeff288@gmail.com>
2021-02-02 14:33:06 -0500

    Object destructuring and spread operator

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -25,7 +25,63 @@ const restaurant = {
   order: function (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
+
+  orderDelivery: function (obj) {
+    console.log(obj);
+  },
 };
+
+restaurant.orderDelivery({
+  time: '22:30',
+  address: 'Via del Sole, 21',
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+//spread operator
+const arr = [7, 8, 9];
+const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+console.log(badNewArr);
+
+const newArr = [1, 2, ...arr];
+console.log(newArr);
+
+console.log(...newArr);
+console.log(1, 2, 7, 8, 9);
+
+/*
+//////////////
+//Destructuring Objects
+
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+console.log(restaurantName, hours, tags);
+
+//default values
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
+
+//mutating variables
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 14 };
+({ a, b } = obj);
+console.log(a, b);
+
+//Nested objects
+const {
+  //fri: { open, close},
+  fri: { open: o, close: c },
+} = openingHours;
+// console.log(open, close);
+console.log(o, c);
+*/
 
 /*
 //destructing arrays
