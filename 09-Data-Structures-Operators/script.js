38ad99dca72ce9cb2478865cc3c3d9e519c92836
Jeff-Peters513 <petersjeff288@gmail.com>
2021-02-18 13:33:54 -0500

    116 maps-fundamentals

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -66,8 +66,43 @@ const restaurant = {
   },
 };
 
-//116 Maps: Fundamentals
+//117 Maps: Iteration
 
+/*
+//116 Maps: Fundamentals
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze Italy');
+console.log(rest.set(2, 'Lisbon Portugal'));
+
+//you can string together the set methods in one call if nessecary
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('closed', 23)
+  .set(true, 'We are open :D')
+  .set(false, 'We are closed :(');
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+const time = 21;
+console.log(rest.get(time > rest.get('open') && time < rest.get('closed')));
+
+console.log(rest.has('categories'));
+rest.delete(2);
+//rest.clear();
+const arr = [1, 2];
+rest.set(arr, 'Test');
+rest.set(document.querySelector('h1'), 'Heading');
+
+console.log(rest);
+console.log(rest.size);
+
+console.log(rest.get(arr));
+
+*/
 /*
 ///////////////////////////////
 //115 Sets (data structure)
