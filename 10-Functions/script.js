c6af508f061f86a8862744b132103a181b4ebdc5
Jeff-Peters513 <petersjeff288@gmail.com>
2021-02-25 14:18:43 -0500

    132 the call and apply methods

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -126,5 +126,61 @@ greet3('Hello', 'Fun Arrow function');
 
 */
 
+/*
 ////////////////
-//131 The Call and Apply Methods
+//132 The Call and Apply Methods
+
+const lufthansa = {
+  airline: 'Lufthansa',
+  iataCode: 'LH',
+  bookings: [],
+  //book: function() {}; full old function method
+  //enhanced object literal shown below
+  book(flightNum, name) {
+    console.log(`${name} booked a seat on ${this.airline} 
+    flight ${this.iataCode}${flightNum}`);
+    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+  },
+};
+
+lufthansa.book(239, 'Jonas Schmedtmann');
+lufthansa.book(635, 'John Smith');
+console.log(lufthansa);
+
+const eurowings = {
+  airline: 'Eurowings',
+  iataCode: 'EW',
+  bookings: [],
+};
+
+const book = lufthansa.book;
+
+//book(23, 'Sarah Williams'); // does not work
+
+//call method - sets the "this" keyword to the proper object
+book.call(eurowings, 23, 'Sarah Williams');
+console.log(eurowings);
+
+book.call(lufthansa, 239, 'Mary Cooper');
+console.log(lufthansa);
+
+const swiss = {
+  airline: 'Swiss Air Lines',
+  iataCode: 'LX',
+  bookings: [],
+};
+
+book.call(swiss, 583, 'Mary Cooper');
+console.log(swiss);
+
+//Apply method
+const flightData = [583, 'George Cooper'];
+book.apply(swiss, flightData);
+console.log(swiss);
+
+//Jonas prefers the call method
+book.call(swiss, ...flightData);
+*/
+
+////////////////////
+//133 The bind method
