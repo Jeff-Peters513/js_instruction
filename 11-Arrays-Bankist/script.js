1f68aca19c701ce171f2d604e18f0440edb0bd4c
Jeff-Peters513 <petersjeff288@gmail.com>
2021-03-02 11:19:53 -0500

    153 lecture and practice and bankist project work

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -71,7 +71,7 @@ const displayMovements = function (movements) {
      <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type} </div>
-     <div class="movements__value">${mov}</div>
+     <div class="movements__value">${mov}€</div>
    </div>`;
 
     containerMovements.insertAdjacentHTML('afterbegin', html);
@@ -81,9 +81,33 @@ displayMovements(account1.movements);
 
 const calcDisplayBalance = function (movements) {
   const balance = movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${balance} EUR`;
+  labelBalance.textContent = `${balance}€`;
 };
 calcDisplayBalance(account1.movements);
+
+const calcDisplaySummary = function (movements) {
+  const incomes = movements
+    .filter(mov => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `${incomes}€`;
+
+  const out = movements
+    .filter(mov => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `${Math.abs(out)}€`;
+
+  const interest = movements
+    .filter(mov => mov > 0)
+    .map(deposit => deposit * (1.2 / 100))
+    .filter((int, _i, arr) => {
+      console.log(arr);
+      return int >= 1;
+    })
+    .reduce((acc, int) => acc + int, 0);
+  labelSumInterest.textContent = `${Math.abs(interest)}€`;
+};
+
+calcDisplaySummary(account1.movements);
 
 //using map method in the project
 const creatUsernames = function (accs) {
@@ -266,6 +290,7 @@ console.log(depositsFor);
 const withdrawals = movements.filter(mov => mov < 0);
 console.log(withdrawals);
 */
+/*
 ///////////////////
 //151 Reduce method
 console.log(movements);
@@ -295,6 +320,19 @@ const max = movements.reduce((acc, mov) => {
   }
 }, movements[0]);
 console.log(max);
-
+*/
 /////////////////////////
-//
+//153 The Magic of Chaining Methods
+const eurToUsd = 1.1;
+console.log(movements);
+
+//PIPELINE
+const totalDepositsUSD = movements
+  .filter(mov => mov > 0)
+  .map((mov, i, arr) => {
+    //console.log(arr);
+    return mov * eurToUsd;
+  })
+  //.map(mov => mov * eurToUsd)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(totalDepositsUSD);
