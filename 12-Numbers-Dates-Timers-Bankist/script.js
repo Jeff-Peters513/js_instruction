a8971453a94f37daf512aa4205aae81dca589e89
Jeff-Peters513 <petersjeff288@gmail.com>
2021-03-14 17:19:41 -0400

    170 the remainder operator

--- 12-Numbers-Dates-Timers-Bankist/script.js
+++ 12-Numbers-Dates-Timers-Bankist/script.js
@@ -290,6 +290,7 @@ console.log(Number.isInteger(23.0));
 console.log(Number.isInteger(23 / 0));
 
 */
+/*
 //169 Math and Rounding
 
 console.log(Math.sqrt(25));
@@ -332,3 +333,24 @@ console.log((2.7).toFixed(0));
 console.log((2.7).toFixed(3));
 console.log((2.345).toFixed(2));
 console.log(+(2.345).toFixed(2)); //add + to cast back to a number
+*/
+
+//170 The Remainder Operator
+console.log(5 % 2);
+console.log(5 / 2); // 5 = 2*2 +1
+
+console.log(8 % 3);
+console.log(8 / 3); // 8 = 2* 3 + 2
+
+console.log(6 % 2); //even it can be divided by 2 with no remainder
+console.log(6 / 2);
+
+console.log(7 % 2);
+console.log(7 / 2);
+
+const isEven = n => n % 2 === 0;
+console.log(isEven(8));
+console.log(isEven(23));
+console.log(isEven(514));
+
+document.querySelectorAll('.movements__row');
