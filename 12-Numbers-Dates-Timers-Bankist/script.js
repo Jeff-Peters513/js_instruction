c2cd12c98b89afb7f2f24e507ae86f255558de02
Jeff-Peters513 <petersjeff288@gmail.com>
2022-02-15 14:10:33 -0500

    small update to 03 skills and 12-Numbers

--- 12-Numbers-Dates-Timers-Bankist/script.js
+++ 12-Numbers-Dates-Timers-Bankist/script.js
@@ -471,8 +471,16 @@ future.setFullYear(2040);
 console.log(future);
 */
 //////
-
 //173 Adding dates to "Bankist" App
 //all work was in the "Bankest App" above
 
 ////174 Operations with dates
+const future = new Date(2037, 10, 19, 15, 23);
+//console.log(Number(future));//long way
+console.log(+future);
+
+const calcdaysPassed = (date1, date2) =>
+  Math.abs(date2 - date1) / (1000 * 60 * 60 * 24);
+
+const days1 = calcdaysPassed(new Date(2037, 3, 4), new Date(2037, 3, 14));
+console.log(days1);
